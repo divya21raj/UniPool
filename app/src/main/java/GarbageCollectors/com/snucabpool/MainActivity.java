4cb6b67ca9d226d067087682f7e99deca527f048
Divya Raj <divya21raj@gmail.com>
2017-11-12 15:26:31 +0530

    Minor fix

--- app/src/main/java/GarbageCollectors/com/snucabpool/MainActivity.java
+++ app/src/main/java/GarbageCollectors/com/snucabpool/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextMessage = (TextView) findViewById(R.id.info_text);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
