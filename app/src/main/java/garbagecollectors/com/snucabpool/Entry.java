0171374a2da8f3ebb98ff1cee860b35f5deb801c
Divya Raj <divya21raj@gmail.com>
2017-11-23 21:11:11 +0530

    Constructor, getters added to Entry class

--- app/src/main/java/garbagecollectors/com/snucabpool/Entry.java
+++ app/src/main/java/garbagecollectors/com/snucabpool/Entry.java
@@ -11,4 +11,41 @@ public class Entry
     private long user_id;                                                 //Data type could be changed to long
     String source, destination,time;
     private HashMap<Long, Float> map = new HashMap<>();                   //HashMap contains entry_id(Long value) and lambda(Float value)
+
+    public Entry(long user_id, String source, String destination, String time, HashMap<Long, Float> map)
+    {
+        this.user_id = user_id;
+        this.source = source;
+        this.destination = destination;
+        this.time = time;
+        this.map = map;
+    }
+
+    public Entry()
+    {}
+
+    public long getUser_id()
+    {
+        return user_id;
+    }
+
+    public String getSource()
+    {
+        return source;
+    }
+
+    public String getDestination()
+    {
+        return destination;
+    }
+
+    public String getTime()
+    {
+        return time;
+    }
+
+    public HashMap<Long, Float> getMap()
+    {
+        return map;
+    }
 }
--- app/src/main/java/garbagecollectors/com/snucabpool/activities/NewEntryActivity.java
+++ app/src/main/java/garbagecollectors/com/snucabpool/activities/NewEntryActivity.java
@@ -31,40 +31,12 @@ public class NewEntryActivity extends BaseActivity  {
     private long user_id;                                                 //Data type could be changed to long
     String source, destination,time;
     String AM_PM ;
-    Button buttonstartSetDialog,buttonChangeDate;
+    Button buttonstartSetDialog,buttonChangeDate, buttonFinalSave;
     TextView text;
     public static String date;
     private HashMap<Long, Float> map = new HashMap<>();                   //HashMap contains entry_id(Long value) and lambda(Float value)
 
     TimePickerDialog timePickerDialog;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_entry);
-
-        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
-        navigationView.setOnNavigationItemSelectedListener(this);
-
-        text = (TextView)findViewById(R.id.searched_address);//Check
-
-        buttonChangeDate = (Button)findViewById(R.id.SetTime);
-        buttonChangeDate.setOnClickListener(new OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                openTimePickerDialog(false);
-            }});
-
-        buttonstartSetDialog = (Button)findViewById(R.id.btnChangeDate);
-        buttonstartSetDialog.setOnClickListener(new OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                DialogFragment newFragment = new DatePickerFragment();
-                newFragment.show(getFragmentManager(),"Date Picker");
-            }});
-    }
-
     @Override
     int getContentViewId()
     {
@@ -77,7 +49,52 @@ public class NewEntryActivity extends BaseActivity  {
         return R.id.navigation_newEntry;
     }
 
-    private void openTimePickerDialog(boolean is24r){
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_entry);
+
+        navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+        text = (TextView)findViewById(R.id.searched_address);//Check
+
+        buttonChangeDate = (Button)findViewById(R.id.SetTime);
+        buttonChangeDate.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                openTimePickerDialog(false);
+            }
+        });
+
+        buttonstartSetDialog = (Button)findViewById(R.id.btnChangeDate);
+        buttonstartSetDialog.setOnClickListener(new OnClickListener(){
+
+            @Override
+            public void onClick(View v)
+            {
+                DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getFragmentManager(),"Date Picker");
+            }
+        });
+
+        buttonFinalSave = (Button)findViewById(R.id.finalSave);
+        buttonFinalSave.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                finalSave(v);
+            }
+        });
+
+    }
+
+    private void openTimePickerDialog(boolean is24r)
+    {
         Calendar calendar = Calendar.getInstance();
 
         timePickerDialog = new TimePickerDialog(
@@ -92,10 +109,12 @@ public class NewEntryActivity extends BaseActivity  {
     }
 
     OnTimeSetListener onTimeSetListener
-            = new OnTimeSetListener(){
+            = new OnTimeSetListener()
+    {
 
         @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+        {
 
             Calendar calNow = Calendar.getInstance();
             Calendar calSet = (Calendar) calNow.clone();
@@ -104,25 +123,30 @@ public class NewEntryActivity extends BaseActivity  {
             calSet.set(Calendar.MINUTE, minute);
             int HourOfDay=calSet.get(Calendar.HOUR_OF_DAY);
             int minOfDay=calSet.get(Calendar.MINUTE);
-            if(hourOfDay < 12) {
+
+            if(hourOfDay < 12)
                 AM_PM = "AM";
-            } else {
+            else
                 AM_PM = "PM";
-            }
+
             time=HourOfDay+":"+minOfDay+" "+AM_PM;
             //text.setText(time);
-        }};
+        }
+    };
 
-    public void findSource(View view) {
+    public void findSource(View view)
+    {
 
         count=0;
-        try {
+        try
+        {
             Intent intent =
                     new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
                             .build(this);
             startActivityForResult(intent, 1);
 
-        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e)
+        {
             // TODO: Handle the error.
         }
 
@@ -130,47 +154,58 @@ public class NewEntryActivity extends BaseActivity  {
 
     // A place has been received; use requestCode to track the request.
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == 1)
+        {
+            if (resultCode == RESULT_OK)
+            {
                 Place place = PlaceAutocomplete.getPlace(this, data);
                 Log.e("Tag", "Place: " + place.getAddress() + place.getPhoneNumber());
-                if(count==0){
+                if(count==0)
+                {
                     source=(place.getName()+",\n"+
                             place.getAddress() +"\n" + place.getPhoneNumber());//check
                     //((TextView) findViewById(R.id.searched_address)).setText(source);
                 }
-                else{
+                else
+                {
                     destination=(place.getName()+",\n"+
                             place.getAddress() +"\n" + place.getPhoneNumber());//check
                     //((TextView) findViewById(R.id.searched_address)).setText(destination);
                 }
 
 
-            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR)
+            {
                 Status status = PlaceAutocomplete.getStatus(this, data);
                 // TODO: Handle the error.
                 Log.e("Tag", status.getStatusMessage());
 
-            } else if (resultCode == RESULT_CANCELED) {
+            } else if (resultCode == RESULT_CANCELED)
+            {
                 // The user canceled the operation.
             }
         }
     }
 
-    public void findDestination(View view) {
+    public void findDestination(View view)
+    {
         count=1;
-        try {
+        try
+        {
             Intent intent =
                     new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
                             .build(this);
             startActivityForResult(intent, 1);
-        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e) {
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException e)
+        {
             // TODO: Handle the error.
         }
     }
 
-    public void finalSave(View view){
-
+    public void finalSave(View view)
+    {
+        
     }
 }
