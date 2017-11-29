831b11e5b1f8c12fa5626431216408d3e0ec71ae
Lord-AJ <atishayjain708@gmail.com>
2017-11-30 02:22:47 +0530

    Displaying Cabpool Requests in app users requestsReceived map

--- app/src/main/java/garbagecollectors/com/snucabpool/activities/RequestActivity.java
+++ app/src/main/java/garbagecollectors/com/snucabpool/activities/RequestActivity.java
@@ -2,16 +2,23 @@ package garbagecollectors.com.snucabpool.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.ArrayList;
+
+import garbagecollectors.com.snucabpool.RequestAdapter;
 import garbagecollectors.com.snucabpool.User;
 
 import garbagecollectors.com.snucabpool.R;
 
-public class RequestActivity extends AppCompatActivity {
+import static garbagecollectors.com.snucabpool.R.styleable.View;
+
+public class RequestActivity extends BaseActivity {
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -32,16 +39,41 @@ public class RequestActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new MyAdapter(myDataset);
+        //DataSnapshot dataSnapshot = new DataSnapshot();
+        /*for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren())
+        {
+            User u = dataSnapshot1.getValue(User.class);
+            // FireModel fire = new FireModel();
+            User fire = new User();
+            String user_Id = u.getUserId();
+            String name = u.getName();
+
+            fire.setUserId(user_Id);
+            fire.setName(name);
+            mDataset.add(fire);
+
+        }*/
+        ArrayList<User> myDataset = new ArrayList<User>();
+        mAdapter = new RequestAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
+        }
 
         //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         //fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            //@Override
+        public void onClick(TextView view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+
+
+    @Override
+    int getContentViewId() {
+        return 0;
     }
-}
+
+    @Override
+    int getNavigationMenuItemId() {
+        return 0;
+    };
+    }
