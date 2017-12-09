4826363ca96db5672aeaeec988327c77be5eab75
Divya Raj <divya21raj@gmail.com>
2017-12-10 00:55:54 +0530

    Create Entry fixed

--- app/src/main/java/garbagecollectors/com/snucabpool/activities/BaseActivity.java
+++ app/src/main/java/garbagecollectors/com/snucabpool/activities/BaseActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import garbagecollectors.com.snucabpool.R;
 import garbagecollectors.com.snucabpool.User;
@@ -22,8 +23,8 @@ public abstract class BaseActivity extends AppCompatActivity implements BottomNa
 
     protected FirebaseAuth mAuth;
     protected static FirebaseUser currentUser;
-    protected static DatabaseReference userDatabaseReference;
-    protected static DatabaseReference entryDatabaseReference;
+    protected static DatabaseReference userDatabaseReference = FirebaseDatabase.getInstance().getReference("users");
+    protected static DatabaseReference entryDatabaseReference = FirebaseDatabase.getInstance().getReference("entries");
     static User finalCurrentUser;
 
 
