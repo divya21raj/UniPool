b2af33d6072177a5ec94bf5b85dc035097006212
Divya Raj <divya21raj@gmail.com>
2017-12-16 19:05:39 +0530

    Minor Changes

--- app/src/main/java/garbagecollectors/com/snucabpool/activities/BaseActivity.java
+++ app/src/main/java/garbagecollectors/com/snucabpool/activities/BaseActivity.java
@@ -13,9 +13,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
 import garbagecollectors.com.snucabpool.R;
 import garbagecollectors.com.snucabpool.TripEntry;
